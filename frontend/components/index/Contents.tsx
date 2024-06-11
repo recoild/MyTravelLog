b34c5f7af19e8fbe530dabfdd6cc374b60aad951
RamRam-V <cajhyung@gmail.com>
2024-06-11 19:51:09 +0900

    커버페이지 gpu 과도 사용 이슈 수정.

--- frontend/components/index/Contents.tsx
+++ frontend/components/index/Contents.tsx
@@ -2,6 +2,7 @@ import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import TypingAnimation from "@/components/ui/typing-animation";
 
 export const Contents: React.FC = () => {
     const words = [
@@ -35,15 +36,13 @@ export const Contents: React.FC = () => {
             <span className="pointer-events-none mb-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-4xl sm:text-7xl font-bold leading-none tracking-tighter text-transparent">
                 자유 여행을 떠나는 길
             </span>
-            <TypewriterEffect words={words} className="text-xl" />
-            {/* <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 space-x-0 sm:space-x-4 mt-10">
-                <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-                    여행 로그 작성
-                </button>
-                <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-                    데모 영상
-                </button>
-            </div> */}
+            {/* <TypewriterEffect words={words} className="text-xl" /> */}
+            <TypingAnimation
+                className="text-xl sm:text-5xl  font-bold text-black dark:text-white"
+                text="여행 계획을 쉽게 공유하는 데일리 트래블"
+                duration={80}
+            />
+
             <div className="mt-10 sm:mt-20 flex flex-col w-[400px] items-stretch">
                 <Link href="/springbootserver/oauth2/authorization/google">
                     <Button className="font-bold w-full mb-5 h-[50px] text-xl">
