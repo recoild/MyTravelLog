225f1ba9d7f78f679c13028952ee6fca8f291c25
RamRam-V <cajhyung@gmail.com>
2024-06-16 18:31:54 +0900

    nginx 의 느린 file io 이슈 해결. 추가로 유저 세션 정보 받아오는 흐름 개선.

--- backend/SpringBootServer/src/main/java/com/ramramv/springbootserver/auth/controller/UserController.java
+++ backend/SpringBootServer/src/main/java/com/ramramv/springbootserver/auth/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ramramv.springbootserver.auth.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,7 +13,9 @@ import com.ramramv.springbootserver.auth.dto.UserInfoResponse;
 import com.ramramv.springbootserver.auth.entity.User;
 import com.ramramv.springbootserver.auth.service.UserService;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,18 +24,37 @@ public class UserController {
 
     @GetMapping("/session")
     public ResponseEntity<UserInfoResponse> getSession(Authentication authentication) {
-        if (authentication.isAuthenticated()) {
-            UserInfoResponse userInfoResponse = new UserInfoResponse();
-            if (authentication.getPrincipal() instanceof OAuth2User oauthUser) {
-                String email = oauthUser.getAttribute("email");
-                userInfoResponse.setEmail(email);
-                User user = userService.getUserByEmail(userInfoResponse.getEmail());
-                userInfoResponse.setNickname(user.getNickname());
-                userInfoResponse.setRole(user.getRole().name());
-                userInfoResponse.setPicture(user.getPicture());
-                return ResponseEntity.ok(userInfoResponse);
+        try {
+            if (authentication == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
             }
+
+            if (authentication.isAuthenticated()) {
+                UserInfoResponse userInfoResponse = new UserInfoResponse();
+                if (authentication.getPrincipal() instanceof OAuth2User oauthUser) {
+                    String email = oauthUser.getAttribute("email");
+                    userInfoResponse.setEmail(email);
+                    User user = userService.getUserByEmail(userInfoResponse.getEmail());
+                    userInfoResponse.setNickname(user.getNickname());
+                    userInfoResponse.setRole(user.getRole().name());
+                    userInfoResponse.setPicture(user.getPicture());
+                    return ResponseEntity.ok(userInfoResponse);
+                }
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
+
+    @GetMapping("/test")
+    public String getMethodName() {
+        return new String("test");
+    }
+
+    @GetMapping("/noauth")
+    public ResponseEntity<String> getNoAuth() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("no auth");
+    }
+
 }
